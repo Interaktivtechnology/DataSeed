4da1e9a929b0f49940a957576844303c146a9427
Demas <demasww@gmail.com>
2017-03-07 22:43:23 +0700

    add react frontpage

---
+++ react_frontend/src/containers/Login.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+
+class Login extends Component {
+    render() {
+    
+        return(
+            <div>
+                Login Page
+            </div>
+        )
+    }
+}
+
+export default (Login)
