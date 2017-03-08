7fa89419fde227e3787f709440b90cd0206bc6f7
Eko Purnomo <eko.purnomo@icloud.com>
2017-03-08 10:45:32 +0700

    Init backend and change styles

--- _frontend/src/containers/App.js
+++ _frontend/src/containers/App.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router' 
+import { Link } from 'react-router'
 
 export default class App extends Component {
-    render() {
-        return(
-            <div>
-                <nav className="navbar navbar-default">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <span className="navbar-brand" href="#">
-                                <Link to="/"><img src="./dist/assets/images/logo.png" /></Link>
-                            </span>
-                        </div>
-                        <ul className="nav navbar-nav">
-                            <li>
-                                <Link to="/login" activeStyle={{color: '#555', backgroundColor: '#e7e7e7'}}>Login</Link>
-                            </li>
-                            <li>
-                                <Link to="/pricing" activeStyle={{color: '#555', backgroundColor: '#e7e7e7'}}>Pricing</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                <div className="panel panel-default">
-                    <div className="panel-body">
-                        { this.props.children }
-                    </div>
-                </div>
+  render() {
+    return(
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <span className="navbar-brand" href="#">
+                  <Link to="/"><img src="./dist/assets/images/logo.png" /></Link>
+              </span>
             </div>
-        )
-    }
+            <ul className="nav navbar-nav">
+              <li>
+                  <Link to="/login" activeStyle={{color: '#555', backgroundColor: '#e7e7e7'}}>Login</Link>
+              </li>
+              <li>
+                  <Link to="/pricing" activeStyle={{color: '#555', backgroundColor: '#e7e7e7'}}>Pricing</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="panel panel-default">
+          <div className="panel-body">
+            { this.props.children }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
--- _frontend/src/containers/HomeTable.js
+++ _frontend/src/containers/HomeTable.js
@@ -3,32 +3,32 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
 import { Form, Input, FormGroup, FormControl, Button } from 'react-bootstrap'
 
 var tables = [
-        {
-            field_name: 'ID',
-            types: "Auto Number",
-            options: "Unique, blank=0%"
-        },
-        {
-            field_name: 'FirstName',
-            types: "First Name",
-            options: "locale=\"id_ID\""
-        },
-        {
-            field_name: 'LastName',
-            types: "Last Name",
-            options: "locale=\"id_ID\""
-        },
-        {
-            field_name: 'Email',
-            types: "Email",
-            options: "blank=0%"
-        },
-        {
-            field_name: 'Gender',
-            types: "Gender",
-            options: "blank=20%"
-        }
-        ];
+  {
+      field_name: 'ID',
+      types: "Auto Number",
+      options: "Unique, blank=0%"
+  },
+  {
+      field_name: 'FirstName',
+      types: "First Name",
+      options: "locale=\"id_ID\""
+  },
+  {
+      field_name: 'LastName',
+      types: "Last Name",
+      options: "locale=\"id_ID\""
+  },
+  {
+      field_name: 'Email',
+      types: "Email",
+      options: "blank=0%"
+  },
+  {
+      field_name: 'Gender',
+      types: "Gender",
+      options: "blank=20%"
+  }
+];
 
 class HomeTable extends Component {
 
@@ -40,50 +40,50 @@ class HomeTable extends Component {
 	  console.log(event.target.value);
 	}
 
-    render() {
-    
-        return(
-        	<div>
-            	<div id="tableData" className="row">
-                    <div className="col-md-6">
-                        <BootstrapTable data={tables} striped={true} hover={true} >
-                          <TableHeaderColumn dataField="field_name" isKey={true} dataAlign="center" dataSort={true}>Field Name</TableHeaderColumn>
-                          <TableHeaderColumn dataField="types" dataSort={true}>Types </TableHeaderColumn>
-                          <TableHeaderColumn dataField="options" >Options</TableHeaderColumn>
-                        </BootstrapTable>
-                    </div>
-                </div>
-                <div className="row">
-		            <div className="col-md-6">
-		                <Button bsStyle="default" className="addBtn" onClick={this.btnClick}>Add New Field</Button>
-		                <Button bsStyle="default" className="importBtn" onClick={this.btnClick}>Import</Button>
-		            </div>
-		        </div>
-		        <div className="row">
-		            <div className="col-md-6">
-						<Form inline>
-							<FormGroup controlId="formInlineRows">
-					      		<FormControl type="text" placeholder="#Number of Rows" />
-						 	</FormGroup>
-						    <FormGroup controlId="formInlineFileName">
-						    	<FormControl type="text" placeholder="File Name" />
-						    </FormGroup> 
-						    <FormGroup controlId="formInlineType">
-						        <FormControl componentClass="select" placeholder="select">
-							      <option value="CSV">CSV</option>
-						          <option value="PDF">PDF</option>
-						          <option value="DOC">DOC</option>
-							  	</FormControl>
-						    </FormGroup>
-						  </Form>
-						  <hr />
-						  <Button bsStyle="primary" className="downloadBtn" onClick={this.btnClick}>Download</Button>
-		            </div>
-		        </div>
-		        
-             </div>
-        )
-    }
+  render() {
+
+    return(
+    	<div>
+      	<div id="tableData" className="row">
+              <div className="col-md-6">
+                  <BootstrapTable data={tables} striped={true} hover={true} >
+                    <TableHeaderColumn dataField="field_name" isKey={true} dataAlign="center" dataSort={true}>Field Name</TableHeaderColumn>
+                    <TableHeaderColumn dataField="types" dataSort={true}>Types </TableHeaderColumn>
+                    <TableHeaderColumn dataField="options" >Options</TableHeaderColumn>
+                  </BootstrapTable>
+              </div>
+          </div>
+          <div className="row">
+          <div className="col-md-6">
+              <Button bsStyle="default" className="addBtn" onClick={this.btnClick}>Add New Field</Button>
+              <Button bsStyle="default" className="importBtn" onClick={this.btnClick}>Import</Button>
+          </div>
+      </div>
+      <div className="row">
+          <div className="col-md-6">
+			<Form inline>
+				<FormGroup controlId="formInlineRows">
+		      		<FormControl type="text" placeholder="#Number of Rows" />
+			 	</FormGroup>
+			    <FormGroup controlId="formInlineFileName">
+			    	<FormControl type="text" placeholder="File Name" />
+			    </FormGroup>
+			    <FormGroup controlId="formInlineType">
+			        <FormControl componentClass="select" placeholder="select">
+				      <option value="CSV">CSV</option>
+			          <option value="PDF">PDF</option>
+			          <option value="DOC">DOC</option>
+				  	</FormControl>
+			    </FormGroup>
+			  </Form>
+			  <hr />
+			  <Button bsStyle="primary" className="downloadBtn" onClick={this.btnClick}>Download</Button>
+          </div>
+      </div>
+
+     </div>
+    )
+  }
 }
 
 export default (HomeTable)
