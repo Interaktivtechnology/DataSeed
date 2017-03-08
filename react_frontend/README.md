54f4c67d914d8cc3d115a4fd41b6a55a8304fda2
Eko Purnomo <eko.purnomo@icloud.com>
2017-03-08 10:20:49 +0700

    Change naming

--- react_frontend/README.md
+++
@@ -1,7 +0,0 @@
-## Dataseed demo
-react+redux+react-router
-
-## How to do
-> npm install
-
-> npm run start
--- react_frontend/src/constants.js
+++
@@ -1,7 +0,0 @@
-/* will update nanti 
-export const INCREASE = 'INCREASE'
-export const DECREASE = 'DECREASE'
-export const LOADDATA = 'LOADDATA'
-export const GETSUCCESS = 'GETSUCCESS'
-export const REFRESHDATA = 'REFRESHDATA'
-*/
